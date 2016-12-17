faca1a51b9b5988873e9024157e24aaa0aeedd63
cbott <cbott6@gmail.com>
2016-12-17 02:34:12 -0500

    improve overall code structure

--- email_scraper.py
+++ email_scraper.py
@@ -1,33 +1,44 @@
+import sys
 from searchumich import getEmail
 
 def format_name(name):
-	""" John Doe* -(junk) --> John Doe"""
-	res = ""
-	for letter in name:
-		if letter.isalpha() or letter.isspace() or letter is ".":
-			res += letter
-		else:
-			break
-	# names should not have more than 3 spaces: "Sir Bob B. Brown"
-	if res.count(" ") > 3:
-		res = ""
-	return res.strip()
+    """ John Doe* -(junk) --> John Doe"""
+    res = ""
+    for letter in name:
+        if letter.isalpha() or letter.isspace() or letter is ".":
+            res += letter
+        else:
+            break
+    # names should not have more than 3 spaces: "Sir Bob B. Brown"
+    if res.count(" ") > 3:
+        res = ""
+    return res.strip()
 
-with open("volunteers.csv", "r") as file:
-	rows = file.readlines()
-	names = []
-	for row in rows:
-		# first column is labels, not names
-		cols = row.split(",")[1:]
-		for col in cols:
-			if col.strip() == "":
-				#ignore empty cells
-				continue
-			name = format_name(col)
-			if(name):
-				names.append(name)
+def scrape_csv(filename):
+    """ Grab names that appear in a csv file, ignoring the first
+        column, and only selecting strings that look like names """
+    with open(filename, "r") as file:
+        rows = file.readlines()
+        names = []
+        for row in rows:
+            # first column is labels, not names
+            cols = row.split(",")[1:]
+            for col in cols:
+                if col.strip() == "":
+                    #ignore empty cells
+                    continue
+                name = format_name(col)
+                if(name):
+                    names.append(name)
+    return names
 
-unique_people = list(set(names))
-unique_people.sort()
-for person in unique_people:
-	print person+","+getEmail(person)
+def remove_duplicates(l):
+    """ Remove duplicates from a list and return sorted """
+    unique = list(set(l))
+    unique.sort()
+    return unique
+
+file = sys.argv[1]
+people = remove_duplicates(scrape_csv(file))
+for person in people:
+        print person+","+"/".join(getEmail(person))
--- searchumich.py
+++ searchumich.py
@@ -1,34 +1,37 @@
 from requests import post
 
-def getEmail(name):
-	url = 'https://mcommunity.umich.edu/mcPeopleService/people/search'
-	query = {"searchCriteria":name}
+def _valid(person):
+    # The person is not an alumnus
+    aff = person.get("affiliation")
+    if aff is None:
+        return False
+    if aff == "Alumni" or (aff[0] == "Alumni" and not person.get("title")):
+        return False
+    return True
 
-	response = post(url, data = query)
-	data = response.json()['person']
-	try:
-		if len(data) == 0:
-			return "UNKNOWN"
-		elif len(data) == 1:
-			return data[0]['email']
-		else:
-			candidates = []
-			for person in data:
-				if person["affiliation"] == "Alumni":
-					continue
-				candidates.append(person)
-			email = candidates[0]["email"]
-			if len(candidates) > 1:
-				email += "*"
-			return email
-	except:
-		return "UNKNOWN+"
+def getEmail(name):
+    url = 'https://mcommunity.umich.edu/mcPeopleService/people/search'
+    query = {"searchCriteria":name}
+
+    response = post(url, data = query)
+    data = response.json()['person']
+
+    results = []
+    try:
+        for person in data:
+            #print person
+            if _valid(person):
+                results.append(str(person["email"]))
+    except Exception as e:
+        results = ["error"]
+    finally:
+        return results
 
 if __name__ == "__main__":
-	import sys
-	if len(sys.argv) < 2:
-		print "Usage: searchumich.py <query>"
-	else:
-		query = ' '.join(sys.argv[1:])
-		print query, ":", getEmail(query)
-	
+    import sys
+    if len(sys.argv) < 2:
+        print "Usage: searchumich.py <query>"
+    else:
+        query = ' '.join(sys.argv[1:])
+        print query, ":", getEmail(query)
+    
